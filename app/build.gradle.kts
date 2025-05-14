a7b8996719387290abfd51a821e74ff119adabac
Roy-Garcia-Rendon <rodrigoa.gr11@gmail.com>
2025-05-14 17:57:20 -0600

    Arreglo de la pantalla de usuario

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,9 @@ dependencies {
     // Lottie for animations
     implementation("com.airbnb.android:lottie:6.3.0")
     
+    // CircleImageView for profile image
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
